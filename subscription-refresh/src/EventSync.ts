4e94a12b650b0dea80c2c86b2a2abc287a72afb5
kadencartwright <cartwrightkaden@gmail.com>
2021-11-03 18:05:18 -0500

    fixed url parsing bug

--- subscription-refresh/src/EventSync.ts
+++ subscription-refresh/src/EventSync.ts
@@ -1,93 +1,119 @@
 import { Client } from "@microsoft/microsoft-graph-client";
-import {MyAuthenticationProvider} from './MyAuthenticationProvider'
-import 'isomorphic-fetch'
+import { MyAuthenticationProvider } from "./MyAuthenticationProvider";
+import "isomorphic-fetch";
 
-const eventSync = async function (webhookData:any, client:Client): Promise<string> {
-    console.log('EVENT SYNC ACTIVITY RUNNING')
-    console.log('getting a token from auth provider')
-    let token = await new MyAuthenticationProvider().getAccessToken();
+const eventSync = async function (
+  webhookData: any,
+  client: Client
+): Promise<string> {
+  console.log("EVENT SYNC ACTIVITY RUNNING");
+  console.log("getting a token from auth provider");
+  let token = await new MyAuthenticationProvider().getAccessToken();
 
-    //assign the input from the orchestrator(Events/index.ts) function to a local variable
-    let changes = webhookData.value;
+  //assign the input from the orchestrator(Events/index.ts) function to a local variable
+  let changes = webhookData.value;
 
-    for (const change of changes) {
-        if (change.clientState = process.env.SUBSCRIPTION_SECRET){
+  for (const change of changes) {
+    if ((change.clientState = process.env.SUBSCRIPTION_SECRET)) {
+      switch (change.changeType) {
+        case "created":
+          await createdHandler(change, client);
+          break;
+        case "updated":
+          //do nothing
+          break;
+        case "deleted":
+          //do nothing
+          break;
+      }
+    }
+  }
 
-            switch(change.changeType){
-                case "created":
-                
-                    await createdHandler(change,client)
-                    break;
-                case "updated":
-                    //do nothing
-                    break;
-                case "deleted":
-                    //do nothing
-                    break;
-            }
-        }
-    };
-
-    //this doesnt need to necessarily return this token, it is for testing purposes
-    return token;
-    
+  //this doesnt need to necessarily return this token, it is for testing purposes
+  return token;
 };
 
+let createdHandler: (change: any, client: Client) => void = async function (
+  change: any,
+  client: Client
+) {
+  //get the id of the calendar the given event is a member of
+  console.log("getting event info from graph");
+  let event = await client.api(change.resource).expand("calendar").get();
+  //extract the name
+  console.log("getting calendar info from graph");
+  let calendarName = (
+    await client
+      .api(
+        `/users/${process.env.CALENDAR_OWNER_UPN}/calendars/${event.calendar.id}`
+      )
+      .get()
+  ).name;
+  //find the group
+  console.log(
+    `getting corresponding group info for calendar named: ${calendarName}`
+  );
+  const calendarNameFormatted: string = calendarName
+    .replace(/\s/g, "")
+    .toLowerCase();
+  console.log(
+    `filtered by: startswith(mail,'${process.env.GROUP_EMAIL_PREPEND}${calendarNameFormatted}@' ) `
+  );
+  let response = await client
+    .api(`/groups?$filter=startswith(mail,'meetingdisttest2@')`)
+    .filter(
+      `startswith(mail,'${process.env.GROUP_EMAIL_PREPEND}${calendarNameFormatted}@' ) `
+    )
+    .get();
 
-    let createdHandler:(change:any,client:Client)=>void = async function(change:any,client:Client){
-        //get the id of the calendar the given event is a member of
-        console.log('getting event info from graph')
-        let event = (await client.api(change.resource).expand('calendar').get())
-        //extract the name
-        console.log('getting calendar info from graph')
-        let calendarName = (await client.api(`/users/${process.env.CALENDAR_OWNER_UPN}/calendars/${event.calendar.id}`).get()).name
-        //find the group
-        console.log(`getting corresponding group info for calendar named: ${calendarName}`)
-        console.log(`filtered by: startswith(mail,'${process.env.GROUP_EMAIL_PREPEND}${calendarName.replace(' ','').toLowerCase()}@' ) `)
-        let response = await client.api(`/groups?$filter=startswith(mail,'meetingdisttest2@')`).filter(`startswith(mail,'${process.env.GROUP_EMAIL_PREPEND}${calendarName.replace(' ','').toLowerCase()}@' ) `).get()
+  console.log("response from graph:");
+  console.log(JSON.stringify(response));
 
-        console.log('response from graph:')
-        console.log(JSON.stringify(response))
+  let group = response.value[0];
+  console.log("groups");
+  console.log(JSON.stringify(group));
 
-        let group = response.value[0]
-        console.log('groups')
-        console.log(JSON.stringify(group))
+  console.log("getting group members");
+  try {
+    let membersResponse = await client
+      .api(`/groups/${group.id}/members`)
+      .select("mail,displayName,id")
+      .get();
+    let attendees = event.attendees;
+    for (let member of membersResponse.value) {
+      if (member["@odata.type"].includes("graph.orgContact")) {
+        //this means member is contact not AD user. need to fetch contact info.\                console.log('member:')
+        console.log(JSON.stringify(member));
+        member = await client
+          .api(`/contacts/${member.id}`)
+          .select("mail,displayName,id")
+          .get();
+      }
+      let found = false;
+      for (const attendee of attendees) {
+        if (member.mail == attendee.emailAddress.address) {
+          found = true;
+          break;
+        }
+      }
 
-        console.log('getting group members')
-       try{     
-            let membersResponse = (await client.api(`/groups/${group.id}/members`).select('mail,displayName,id').get())
-            let attendees = event.attendees;
-            for (let member of membersResponse.value){
-                if (member['@odata.type'].includes('graph.orgContact')){
-                    //this means member is contact not AD user. need to fetch contact info.\                console.log('member:')
-                    console.log(JSON.stringify(member))
-                    member = (await client.api(`/contacts/${member.id}`).select('mail,displayName,id').get())
-                }
-                let found = false
-                for (const attendee of attendees){
-                    if (member.mail == attendee.emailAddress.address){
-                        found = true
-                        break
-                    }
-                }
+      if (!found) {
+        console.log(`added attendee: ${member.mail}`);
 
-                if (!found ){
-                    console.log(`added attendee: ${member.mail}`)
-
-                    attendees.push({emailAddress:{address:member.mail,name:member.displayName}})
-                }
-            }
-            console.log('updating attendees')
-            await client.api(`/users/${process.env.CALENDAR_OWNER_UPN}/events/${event.id}?$select=id`)
-                            .update({attendees:[...event.attendees,...attendees]})
-        }catch(e){
-                console.log(e)
-            }
-        
+        attendees.push({
+          emailAddress: { address: member.mail, name: member.displayName },
+        });
+      }
     }
-
-
+    console.log("updating attendees");
+    await client
+      .api(
+        `/users/${process.env.CALENDAR_OWNER_UPN}/events/${event.id}?$select=id`
+      )
+      .update({ attendees: [...event.attendees, ...attendees] });
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 export default eventSync;
-
-
--- subscription-refresh/src/Helpers.ts
+++ subscription-refresh/src/Helpers.ts
@@ -1,71 +1,93 @@
-import { Client, GraphRequest } from '@microsoft/microsoft-graph-client';
-import { MyAuthenticationProvider } from './MyAuthenticationProvider';
-async function syncAllEventsInCalendar(calendarId:String,client:Client){
+import { Client, GraphRequest } from "@microsoft/microsoft-graph-client";
+import { MyAuthenticationProvider } from "./MyAuthenticationProvider";
+async function syncAllEventsInCalendar(calendarId: String, client: Client) {
+  console.log("getting calendar info from graph");
+  let calendarName = (
+    await client
+      .api(`/users/${process.env.CALENDAR_OWNER_UPN}/calendars/${calendarId}`)
+      .get()
+  ).name;
+  //find the group
+  try {
+    console.log("getting corresponding group info");
+    let group = (
+      await client
+        .api(`/groups`)
+        .filter(
+          `startswith(mail,'${process.env.GROUP_EMAIL_PREPEND}${calendarName
+            .replace(" ", "")
+            .toLowerCase()}@' ) `
+        )
+        .get()
+    ).value[0];
+    console.log("group found:", { group });
 
-    console.log('getting calendar info from graph')
-    let calendarName = (await client.api(`/users/${process.env.CALENDAR_OWNER_UPN}/calendars/${calendarId}`).get()).name
-    //find the group
-    try{    
-        console.log('getting corresponding group info')
-        let group = (await client.api(`/groups`).filter(`startswith(mail,'${process.env.GROUP_EMAIL_PREPEND}${calendarName.replace(' ','').toLowerCase()}@' ) `).get()).value[0]
-
-        let events= await getAllPagesFromGraph(client.api(`/users/${process.env.CALENDAR_OWNER_UPN}/calendars/${calendarId}/events`))
-        console.log('getting group members')
-        let groupMembers = (await getAllPagesFromGraph(client.api(`/groups/${group.id}/members`).select('mail,displayName,id')))
-        for (const event of events){
-            let attendees = []
-            for (let groupMember of groupMembers){
-                if (groupMember['@odata.type'].includes('graph.orgContact')){
-                    //this means member is contact not AD user. need to fetch contact info.\                console.log('member:')
-                    console.log(JSON.stringify(groupMember))
-                    groupMember = (await client.api(`/contacts/${groupMember.id}`).select('mail,displayName,id').get())
-                }
-                let found = false
-                for (const attendee of event.attendees){
-                    if (groupMember.mail == attendee.emailAddress.address){
-                        found = true
-                        break
-                    }
-                }
-                if (!found){
-                    attendees.push({emailAddress:{address:groupMember.mail,name:groupMember.displayName}})
-                }
-            }
-            console.log('updating attendees')
-            await client.api(`/users/${process.env.CALENDAR_OWNER_UPN}/events/${event.id}?$select=id`)
-                            .update({attendees:[...event.attendees,...attendees]})
+    let events = await getAllPagesFromGraph(
+      client.api(
+        `/users/${process.env.CALENDAR_OWNER_UPN}/calendars/${calendarId}/events`
+      )
+    );
+    console.log("getting group members");
+    let groupMembers = await getAllPagesFromGraph(
+      client.api(`/groups/${group.id}/members`).select("mail,displayName,id")
+    );
+    for (const event of events) {
+      let attendees = [];
+      for (let groupMember of groupMembers) {
+        if (groupMember["@odata.type"].includes("graph.orgContact")) {
+          //this means member is contact not AD user. need to fetch contact info.\                console.log('member:')
+          console.log(JSON.stringify(groupMember));
+          groupMember = await client
+            .api(`/contacts/${groupMember.id}`)
+            .select("mail,displayName,id")
+            .get();
         }
-        
-    }catch(e){
-            console.log(e)
+        let found = false;
+        for (const attendee of event.attendees) {
+          if (groupMember.mail == attendee.emailAddress.address) {
+            found = true;
+            break;
+          }
         }
-    
-}
-
-async function getAllPagesFromGraph(graphRequest:GraphRequest){
-    let morePages = false;
-    let allPages = []
-    let result  = (await graphRequest.get())
-    allPages.push(...result.value)
-    '@odata.nextLink' in result ? morePages = true: morePages = false;
-    while(morePages){
-            //create an auth provider to pass to the GraphClient (see microsoft graph javascript SDK for information)
-        const options = {
-            authProvider: new MyAuthenticationProvider()
-        };
-        //init the graphClient with our auth provider
-        const client = await Client.initWithMiddleware(options);
-        result = (await client.api(result['@odata.nextLink']).get())
-        allPages.push(...result.value)
-        '@odata.nextLink' in result ? morePages = true: morePages = false;
+        if (!found) {
+          attendees.push({
+            emailAddress: {
+              address: groupMember.mail,
+              name: groupMember.displayName,
+            },
+          });
+        }
+      }
+      console.log("updating attendees");
+      await client
+        .api(
+          `/users/${process.env.CALENDAR_OWNER_UPN}/events/${event.id}?$select=id`
+        )
+        .update({ attendees: [...event.attendees, ...attendees] });
     }
-    return allPages
-
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-
-
-export {
-    syncAllEventsInCalendar,
-    getAllPagesFromGraph
+async function getAllPagesFromGraph(graphRequest: GraphRequest) {
+  let morePages = false;
+  let allPages = [];
+  let result = await graphRequest.get();
+  allPages.push(...result.value);
+  "@odata.nextLink" in result ? (morePages = true) : (morePages = false);
+  while (morePages) {
+    //create an auth provider to pass to the GraphClient (see microsoft graph javascript SDK for information)
+    const options = {
+      authProvider: new MyAuthenticationProvider(),
+    };
+    //init the graphClient with our auth provider
+    const client = await Client.initWithMiddleware(options);
+    result = await client.api(result["@odata.nextLink"]).get();
+    allPages.push(...result.value);
+    "@odata.nextLink" in result ? (morePages = true) : (morePages = false);
+  }
+  return allPages;
 }
+
+export { syncAllEventsInCalendar, getAllPagesFromGraph };
